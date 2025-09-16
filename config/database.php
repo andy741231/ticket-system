497bb997eabd8b21c50d82ebd05156f32ff030ae
Chan <mchan3@Executives-MacBook-Pro-3.local>
2025-09-16 14:49:09 -0500

    ready for new version

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'directory',
+            'database' => env('DB_DATABASE', 'test_ticket'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
